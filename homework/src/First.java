bf52e954a509c220e335a0c55edc4f5249670d91
dmitry <leuropeec@ya.ru>
2020-09-09 18:51:14 +0300

    Доделаны несколько моментов для задания 1

--- homework/src/First.java
+++ homework/src/First.java
@@ -9,9 +9,13 @@ public class First
         System.out.print("Введите количество пользователей: ");
         count = S.nextInt();
 
+        while (!(count > 0 )){
+            System.out.println("Странное количество пользвателей. Может еще раз попробуешь?");
+            System.out.print("Введите количество пользователей: ");
+            count = S.nextInt();
+        }
         Person[] persons = new Person[count];
-        String tempName, tempNum, tempModel;
-        int tempAge;
+        String tempNum;
 
         // Добавление новых пользователей
         for (int i = 0; i < count; i++){
@@ -56,11 +60,51 @@ public class First
             System.out.println(persons[i].name + " добавлен!");
         }
 
-        for (int i = 0; i < count; i++){
+        Scanner T = new Scanner(System.in);
+        int var = 1;
+        do {
+            System.out.println("------------------------------");
+            System.out.println("То, что я могу сделать:");
+            System.out.println("\t1)Вывести информацию о всех пользователях");
+            System.out.println("\t2)Вывести информацию о количестве пользователей");
+            System.out.println("\t3)Поменять данные о телефоне для пользователя");
+            System.out.print("Введите цифру, которая тебя интересует или 0 - для выхода: ");
+            var = T.nextInt();
 
-            System.out.println("-------------");
-            persons[i].displayInfo();
-        }
+            switch (var) {
+                case 0:
+                    System.out.println("Выходим..");
+                    return;
+                //        Выводим информацию о всех пользователях
+                case 1:
+                    for (int i = 0; i < count; i++) {
+
+                        System.out.println("\n-------------");
+                        persons[i].displayInfo();
+                    }
+                    break;
+                //        Выводим информацию о количестве пользователей
+                case 2:
+                    System.out.println("Всего пользователей: " + count);
+                    break;
+                //        Меняем данные о телефоне для пользователей
+                case 3:
+                    System.out.println("------------------------------");
+                    System.out.println("У кого меняем?");
+                    for (int i = 0; i < count; i++) {
+                        System.out.println("\t" + (i + 1) + ") " + persons[i].name + ", номер телефона: " + persons[i].num);
+                    }
+                    System.out.println("Введите цифру, либо 0 - для выхода");
+
+                    var = T.nextInt();
+                    persons[var - 1].changeAboutPhone();
+
+                    break;
+                default:
+                    System.out.println("Я еще не научился");
+                    break;
+            }
+        } while (var != 0);
     }
 }
 
@@ -80,28 +124,68 @@ class Person{
         if (checkCorrectNum(num)) {
             this.num = num;
         } else {
-            this.num = "nill";
+            this.num = "NuN";
         }
     }
 
     // Дефолтные значения
     Person(){
-        this("", 19, "iOS Device", "NuN");
+        this("NuN", 0, "NuN", "NuN");
     }
-    // Конструктор без модели телефона
-    Person(String name, int age, String num){
-        this(name,age,"nill", num);
+
+    void changeAboutPhone(){
+        System.out.println("\n------------------------------");
+        System.out.println("Изменение информации о телефоне ");
+        System.out.println("Старый номер телефона: " + this.num);
+        System.out.print("\tМеняем? 1 - да, 0 - нет");
+
+        Scanner T = new Scanner(System.in);
+        switch (T.nextInt()){
+            case 1:
+                this.changeNum();
+                break;
+            default:
+                System.out.println("Хорошо, дальше");
+                break;
+        }
+
+        System.out.println("Старая модель телефона: " + this.model);
+        System.out.print("\tМеняем? 1 - да, 0 - нет: ");
+
+        switch (T.nextInt()){
+            case 1:
+                this.changeModel();
+                break;
+            default:
+                break;
+        }
+        System.out.println("Редактирование завершено!");
     }
 
     // Изменить номер телефона
-    void changeNum(String num){
-        if (checkCorrectNum(num)){
-            this.num = num;
-            System.out.println(this.name + " изменил номер телефона");
-        } else {
-            this.num = "nill";
-            System.out.println(this.name + " ввел некорректный номер телефона");
+    void changeNum(){
+        System.out.println("Изменение номера телефона");
+        System.out.print("\tВведите новый номер телефона: ");
+        Scanner S = new Scanner(System.in);
+        String num = S.nextLine();
+
+        if (!checkCorrectNum(num)) {
+            while (!checkCorrectNum(num)) {
+                System.out.println("\tОшибка! Проверьте правильность введеных данных");
+                System.out.print("\tВведите номер телефона еще раз: ");
+                num = S.nextLine();
+            }
         }
+        this.num = num;
+    }
+
+    // Меняем модель телефона
+    void changeModel(){
+        System.out.println("Изменение модели телефона");
+        System.out.print("\tВведите новую модель телефона: ");
+        Scanner S = new Scanner(System.in);
+        String model = S.nextLine();
+        this.model = model;
     }
 
     // Вывести данные
@@ -109,6 +193,7 @@ class Person{
         System.out.println("Name: " + this.name + ", " + this.age + " y.o");
         System.out.println("Model: " + this.model + ", " + this.num);
     }
+
     // Проверка правильности номера
     boolean checkCorrectNum(String num){
         if (num.length() != 12) {
